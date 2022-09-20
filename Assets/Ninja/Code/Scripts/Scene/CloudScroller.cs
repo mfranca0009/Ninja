d7ad382964394e19af721504fa661428ea0273c8
Thill020 <94396087+Thill020@users.noreply.github.com>
2022-09-19 23:19:50 -0500

    Added Foliage, walls, and mountains, changed how clouds work.
    -Added Variety of trees to each segment. The number of tress in each segment is representative of what segment number it is. Tress were not overloaded, in order to represent the start of a forest. (NOTE: More trees will be added to each level to show the progression of getting deeper in the forest)
    -Reduced the number of clouds to reduce object clutter.
    -Changed Cloud Scroller script to work off of the player's x location.
    -Removed the wall from segment 1-6
    -Added a wall that is not part of a segment, but goes behind segments 1-6 and 1-7.

--- Assets/Ninja/Code/Scripts/Scene/CloudScroller.cs
+++ Assets/Ninja/Code/Scripts/Scene/CloudScroller.cs
@@ -8,14 +8,15 @@ public class CloudScroller : MonoBehaviour
     [Tooltip("This value deternmines how fast the object scrolls accross the screen. Negitive values moves it right.")]
     public float Speed = 0.0f;
 
-    private GameObject asset;
+    [Tooltip("Player is the object the clouds should be \"orbiting\"")]
+    public GameObject player;
+
     private float step = 0.0f;
     private Vector3 targetPoint; 
 
     // Start is called before the first frame update
     void Start()
     {
-        asset = this.gameObject;
         step = Speed * Time.deltaTime;
         targetPoint = new Vector3(-99, transform.position.y, transform.position.z);
     }
@@ -24,5 +25,21 @@ public class CloudScroller : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPoint, step);
+
+        //If off the left side of the screen, move cloud to off the right side of the screen. 
+        if(transform.position.x < player.transform.position.x - 15)
+        {
+            transform.position = new Vector3(
+                player.transform.position.x + 15, 
+                transform.position.y, 
+                transform.position.z);
+        }
+        else if (transform.position.x > player.transform.position.x + 16)
+        {
+            transform.position = new Vector3(
+                player.transform.position.x - 14,
+                transform.position.y,
+                transform.position.z);
+        }
     }
 }
