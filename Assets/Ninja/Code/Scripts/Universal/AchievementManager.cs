41dae0dfe087bce08444660b65cdf72d159131dd
mfranca915 <mfranca915@gmail.com>
2022-10-13 00:09:47 -0400

    Updates to achievement manager

--- Assets/Ninja/Code/Scripts/Universal/AchievementManager.cs
+++ Assets/Ninja/Code/Scripts/Universal/AchievementManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,33 +5,36 @@ using UnityEngine;
 
 public class Achievement
 {
-    //Used to restart the Achievement when going to the main menu or restarting the game.
-    public Achievement (string topDesc, string botDesc)
+    public Achievement (AchievementType type, string title, string description)
     {
-        TopDescription = topDesc;
-        BottomDescription = botDesc;
+        Type = type;
+        Title = title;
+        Description = description;
         Obtained = false;
-        Elegable = true;
+        Eligible = true;
     }
 
     //Used to restart the Achievement when going to the main menu or restarting the game.
     public Achievement(Achievement achi)
     {
-        TopDescription = achi.TopDescription;
-        BottomDescription = achi.BottomDescription;
+        Type = achi.Type;
+        Title = achi.Title;
+        Description = achi.Description;
         Obtained = false;
-        Elegable = true;
-    }    
+        Eligible = true;
+    }
 
+    public AchievementType Type { get; set; }
+    
     /// <summary>
     /// The top line of the achievement, aka the title.
     /// </summary>
-    public string TopDescription { get; set; }
+    public string Title { get; set; }
 
     /// <summary>
     /// The fulfillment criteria for the achievement. What the player needs to do.
     /// </summary>
-    public string BottomDescription { get; set; }
+    public string Description { get; set; }
 
     /// <summary>
     /// Determines if the player has achieved the achievement or not.
@@ -42,19 +44,20 @@ public class Achievement
     /// <summary>
     /// Determines if the player is allowed to get the achievement during the active run.
     /// </summary>
-    public bool Elegable{ get; set; }
+    public bool Eligible{ get; set; }
 }
 
 public class SpeedBasedAchievement : Achievement
 {
-    public SpeedBasedAchievement(string topDesc, string botDesc, float timer) : base(topDesc, botDesc)
+    public SpeedBasedAchievement(AchievementType type, string title, string description, float timer) : base(type,
+        title, description)
     {
         TimeToBeat = timer;
         TimeElapsed = 0.0f;
     }
 
     //Used to restart the Achievement when going to the main menu or restarting the game.
-    public SpeedBasedAchievement(SpeedBasedAchievement sAchi) : base(sAchi.TopDescription, sAchi.BottomDescription)
+    public SpeedBasedAchievement(SpeedBasedAchievement sAchi) : base(sAchi.Type, sAchi.Title, sAchi.Description)
     {
         TimeToBeat = sAchi.TimeToBeat;
         TimeElapsed = 0.0f;
@@ -71,17 +74,17 @@ public class SpeedBasedAchievement : Achievement
     public float TimeElapsed { get; set; }
 }
 
-public class CollectablesAchievement 
-    : Achievement
+public class CollectiblesAchievement : Achievement
 {
-    public CollectablesAchievement(string topDesc, string botDesc, int NumberOfCollectablesToObtains) : base(topDesc, botDesc)
+    public CollectiblesAchievement(AchievementType type, string title, string description,
+        int numberOfCollectablesToObtains) : base(type, title, description)
     {
-        ObtainableCollectables = NumberOfCollectablesToObtains;
+        ObtainableCollectables = numberOfCollectablesToObtains;
         CollectablesObtained = 0;
     }
 
     //Used to restart the Achievement when going to the main menu or restarting the game.
-    public CollectablesAchievement(CollectablesAchievement cAchi) : base(cAchi.TopDescription, cAchi.BottomDescription)
+    public CollectiblesAchievement(CollectiblesAchievement cAchi) : base(cAchi.Type, cAchi.Title, cAchi.Description)
     {
         ObtainableCollectables = cAchi.ObtainableCollectables;
         CollectablesObtained = 0;
@@ -102,80 +105,110 @@ public class CollectablesAchievement
 
 public class AchievementManager : MonoBehaviour
 {
-    #region Achievement Declaration
+    #region Public Properties
 
-    //1. Entering the Jungle - Clear level 1
-    public Achievement clearLevel1 = new Achievement("Enter the Jungle", "Clear level 1");
-
-    //2. Jungle with a View - Clear level 2
-    public Achievement clearLevel2 = new Achievement("Jungle with a View", "Clear level 2");
-
-    //3. Source of the Coruption - Clear Level 3
-    public Achievement clearLevel3 = new Achievement("Source of the Coruption", "Clear Level 3");
-
-    //4. Quick Ninja - Clear level 1 in under X:XX minutes
-    public SpeedBasedAchievement speedClear1 = new SpeedBasedAchievement("Quick Ninja", "Clear level 1 in under 1:30 minutes", 90.0f);
-
-    //5. Hasty Ninja - Clear level 2 in under X:XX minutes
-    public SpeedBasedAchievement speedClear2 = new SpeedBasedAchievement("Hasty Ninja", "Clear level 2 in under 1:30 minutes", 90.0f);
-
-    //6. Untrackable Ninja - Clear level 3 in under X:XX minutes
-    public SpeedBasedAchievement speedClear3 = new SpeedBasedAchievement("Untrackable Ninja", "Clear level 3 in under 1:30 minutes", 90.0f);
-
-    //7. Coup de Grace - Defeat White Face in under XX Seconds
-    public SpeedBasedAchievement speedClearBoss = new SpeedBasedAchievement("Coup de Grace", "Defeat White Face in under 45 Seconds", 45.0f);
-
-    //8. The Coruption Lingers - Obtain the Bad Ending
-    public Achievement badEnding = new Achievement("The Coruption Lingers", "Obtain the Bad Ending");
-
-    //9. The Coruption is Cleansed - Obtain the Good Ending
-    public CollectablesAchievement goodEnding = new CollectablesAchievement("The Coruption is Cleansed", "Obtain the Good Ending", 3);
-
-    //10. Level 1 Genocide - Kill all enemies in Level 1
-    public CollectablesAchievement genecide1 = new CollectablesAchievement("Level 1 Genocide", "Kill all enemies in Level 1", 8);
-
-    //11. Level 2 Genocide - Kill all enemies in Level 2
-    public CollectablesAchievement genecide2 = new CollectablesAchievement("Level 2 Genocide", "Kill all enemies in Level 2", 7);
-
-    //12. Level 3 Genocide - Kill all enemies in Level 3
-    public CollectablesAchievement genecide3 = new CollectablesAchievement("Level 3 Genocide", "Kill all enemies in Level 3", 11);
-
-    //13. Level 1 Mostly Pasifist - Only Kill Daichi in Level 1
-    public Achievement pacifist1 = new Achievement("Level 1 Mostly Pasifist", "Only Kill Daichi in Level 1");
-
-    //14. Level 2 Mostly Pasifist - Only Kill Renu in Level 1
-    public Achievement pacifist2 = new Achievement("Level 2 Mostly Pasifist", "Only Kill Renu in Level 1");
-
-    //15. Level 3 Mostly Pasifist - Only Kill Bill in Level 1
-    public Achievement pacifist3 = new Achievement("Level 3 Mostly Pasifist", "Only Kill Bill in Level 1");
-
-    //16. Not an Italian Plumber - Jump on an enemies head
-    public Achievement mario = new Achievement("Not an Italian Plumber", "Jump on an enemies head");
-
-    //17. Being a Bully - Push an enemy into a pit
-    public Achievement bully = new Achievement("Being a Bully", "Push an enemy into a pit");
-
-    //18. No Traps Activated - Clear the game without activating any swinging traps
-    public Achievement trapless = new Achievement("No Traps Activated", "Clear the game without activating any swinging traps");
-
-    //19. Expert Ninja - Clear the game without losing any lives
-    public Achievement noLifeLoss = new Achievement("Expert Ninja", "Clear the game without losing any lives");
-
-    //20. Proud Ninja - clear game without grabing any pick-ups
-    public Achievement noPickups = new Achievement("Proud Ninja", "clear game without grabing any pick-ups");
-
-    //21. Resourceful Ninja - Grab a total of 50 pick-ups throughout your journey
-    public CollectablesAchievement yesPickups = new CollectablesAchievement("Resourceful Ninja", "Grab a total of 50 pick-ups throughout your journey", 50);
-
-    //22. Martial Ninja - Beat the game without throwing any knives
-    public Achievement martialNinja = new Achievement("Martial Ninja", "Beat the game without throwing any knives");
-
-    //23. Distance Ninja - Beat the game without using any melee attacks
-    public Achievement distanceNinja = new Achievement("Distance Ninja", "Beat the game without using any melee attacks");
-
-    //24. Master Ninja - Obtain all other Achievements
-    public Achievement allClear = new Achievement("Master Ninja", "Obtain allother Achievements");
+    public List<Achievement> Achievements { get; private set; }
 
     #endregion
 
+    private void Awake()
+    {
+        Achievements = new List<Achievement>();
+        InitAchievements();
+    }
+
+
+    private void InitAchievements()
+    {
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Enter the Jungle", "Clear level 1"));
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Jungle with a View", "Clear level 2"));
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Source of the corruption", "Clear level 3"));
+
+        Achievements.Add(new SpeedBasedAchievement(AchievementType.SpeedType, "Quick Ninja",
+            "Clear level 1 in 1:30 minutes", 90f));
+        Achievements.Add(new SpeedBasedAchievement(AchievementType.SpeedType, "Hasty Ninja",
+            "Clear level 2 in under 1:30 minutes", 90f));
+        Achievements.Add(new SpeedBasedAchievement(AchievementType.SpeedType, "Un-trackable Ninja",
+            "Clear level 3 in under 1:30 minutes", 90f));
+        Achievements.Add(new SpeedBasedAchievement(AchievementType.SpeedType, "Coup de Grâce",
+            "Defeat White Face in under 45 Seconds", 45f));
+
+        Achievements.Add(
+            new Achievement(AchievementType.TriggerType, "The Corruption Lingers", "Obtain the bad ending"));
+        Achievements.Add(new CollectiblesAchievement(AchievementType.CollectibleType, "The corruption is cleansed",
+            "Obtain the good ending", 3));
+
+        Achievements.Add(new CollectiblesAchievement(AchievementType.CollectibleType, "Level 1 Genocide",
+            "Kill all enemies in level 1", 8));
+
+        Achievements.Add(new CollectiblesAchievement(AchievementType.CollectibleType, "Level 2 Genocide",
+            "Kill all enemies in Level 2", 7));
+
+        Achievements.Add(new CollectiblesAchievement(AchievementType.CollectibleType, "Level 3 Genocide",
+            "Kill all enemies in Level 3", 11));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Level 1 Mostly Pacifist",
+            "Only kill Daichi in level 1"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Level 2 Mostly Pacifist",
+            "Only kill Renu in level 2"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Level 3 Mostly Pacifist",
+            "Only kill Bill in level 3"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Not an Italian Plumber",
+            "Jump on an enemy's head"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Being a Bully", "Push an enemy into a pit"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "No Traps Activated",
+            "Clear the game without activating any swinging traps"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Expert Ninja",
+            "Clear the game without losing any lives"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Proud Ninja",
+            "Clear game without grabbing any pick-ups"));
+
+        Achievements.Add(new CollectiblesAchievement(AchievementType.CollectibleType, "Resourceful Ninja",
+            "Grab a total of 50 pick-ups throughout your journey", 50));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Martial Ninja",
+            "Beat the game without throwing any knives"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Distance Ninja",
+            "Beat the game without using any melee attacks"));
+
+        Achievements.Add(new Achievement(AchievementType.TriggerType, "Master Ninja", "Obtain all other Achievements"));
+
+        /*-----------------------------------------------------------------------------------/
+        |                                                                                    |
+        |   Filter out non-trigger type achievements, only select trigger type achievements  |
+        |                                                                                    |
+        |-----------------------------------------------------------------------------------*/
+
+        /*Achievement[] triggerAchievementArray = Achievements.FindAll(possibleAchievements =>
+            possibleAchievements.Type == AchievementType.TriggerType).ToArray();*/
+
+
+        /*-------------------------------------------------------/
+        |                                                        |
+        |  Search for a specific achievement by title and type   |
+        |                                                        |
+        |-------------------------------------------------------*/
+
+        /*Achievement achievementFound = Achievements.Find(possibleAchievement =>
+            possibleAchievement.Type == AchievementType.TriggerType &&
+            possibleAchievement.Title == "Expert Ninja");*/
+
+        /*-------------------------------------------------------/
+        |                                                        |
+        |        Search for an achievement by title              |
+        |                                                        |
+        |-------------------------------------------------------*/
+
+        /*SpeedBasedAchievement achi =
+            Achievements.Find(possibleAchievement =>
+                possibleAchievement.Title == "Enter the Jungle") as SpeedBasedAchievement;*/
+    }
 }
