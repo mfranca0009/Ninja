b8137a8c54f0fa503e78c1657aede84d3e0af3a9
mfranca915 <mfranca915@gmail.com>
2022-10-19 01:26:37 -0400

    Finalize first clean-up pass and adjustments

--- Assets/Ninja/Code/Scripts/Scene/LoadSceneTimer.cs
+++ Assets/Ninja/Code/Scripts/Scene/LoadSceneTimer.cs
@@ -1,26 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadSceneTimer : MonoBehaviour
 {
+    #region Public Properties
+    
+    public int StartLoadSceneTimer { get; set; }
+
+    #endregion
+    
+    #region Serialized Fields
+    [Header("Load Scene Timer Settings")]
+    
+    [Tooltip("The delay before loading the specified scene")]
     [SerializeField] private float waitToLoadScene = 60.0f;
-    private float timeElapsed;
+    
+    [Tooltip("The scene name to load when the timer expires")]
     [SerializeField] private string sceneToLoad;
-    private SceneManagement sceneManagement;
+    
+    #endregion
+
+    #region Private Fields
+
+    private float _waitToLoadSceneTimer;
+    private SceneManagement _sceneManagement;
+    
+    #endregion
+
+    #region Unity Events
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        sceneManagement = FindObjectOfType<SceneManagement>();
+        _sceneManagement = FindObjectOfType<SceneManagement>();
+    }
+
+    private void OnEnable()
+    {
+        _waitToLoadSceneTimer = waitToLoadScene;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        timeElapsed += Time.deltaTime;
-        if (timeElapsed > waitToLoadScene)
-        {
-            sceneManagement.LoadSceneByString(sceneToLoad);
-        }
+        if (StartLoadSceneTimer != 1)
+            return;
+        
+        UpdateLoadSceneTimer();
     }
+    
+    #endregion
+    
+    #region Update Methods
+
+    private void UpdateLoadSceneTimer()
+    {
+        if (_waitToLoadSceneTimer <= 0)
+        {
+            _sceneManagement.LoadSceneByString(sceneToLoad);
+            _waitToLoadSceneTimer = waitToLoadScene;
+        }
+        else
+            _waitToLoadSceneTimer -= Time.deltaTime;
+    }
+    
+    #endregion
 }
