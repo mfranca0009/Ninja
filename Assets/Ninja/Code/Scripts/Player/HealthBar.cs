0f4c892cb0fca453b9213f09c307d7ae98457416
mfranca915 <mfranca915@gmail.com>
2022-10-18 23:14:48 -0400

    Fix runtime errors and apply adjustments to credits scene.

--- Assets/Ninja/Code/Scripts/Player/HealthBar.cs
+++ Assets/Ninja/Code/Scripts/Player/HealthBar.cs
@@ -52,11 +52,14 @@ public class HealthBar : MonoBehaviour
         {
             case true when !_gameManager:
                 return;
-            case true when !_sceneManagement.HasBuildIndex(SceneManager.GetActiveScene(), 0) && !_health:
+            case true when !_sceneManagement.HasBuildIndex(SceneManager.GetActiveScene(), 0, 5) && !_health:
                 _health = _gameManager.Player.GetComponent<Health>();
                 break;
         }
 
+        if ((isPlayer && !_health) || (!isPlayer && !enemyHealth))
+            return;
+        
         healthBar.fillAmount = isPlayer switch
         {
             true => _health.HealthPoints / _health.maxHealth,
